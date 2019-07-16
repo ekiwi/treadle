e67e32c13145c257ec81237515170b52beda5008
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-07-16 15:45:21 -0700

    wip

---
+++ src/main/scala/treadle/symbolic/SymbolicContext.scala
@@ -0,0 +1,19 @@
+// See LICENSE for license details.
+
+package treadle.symbolic
+
+import net.sf.javabdd.JFactory
+
+
+class SymbolicContext {
+
+  private val bdds = JFactory.init(100, 100)
+
+  def makeSymbolic(name: String, unique: Boolean = true) = {
+
+  }
+
+}
+
+
+//case class
---
+++ src/main/scala/treadle/symbolic/ValueSummary.scala
@@ -0,0 +1,11 @@
+// See LICENSE for license details.
+
+package treadle.symbolic
+
+class ValueSummary(val ctx: SymbolicContext) {
+
+
+  //case class Entry(guard: )
+}
+
+
