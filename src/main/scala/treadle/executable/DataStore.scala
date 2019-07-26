107bdf93dc5ebc2760b46cba3384cf5b1b4ff3ab
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-07-26 15:12:56 -0700

    DataStore: move setExecutionEngine into base trait

--- src/main/scala/treadle/executable/DataStore.scala
+++ src/main/scala/treadle/executable/DataStore.scala
@@ -23,49 +23,9 @@ class DataStore(val numberOfBuffers: Int, dataStoreAllocator: DataStoreAllocator
 extends HasDataArrays with AbstractDataStore {
   assert(numberOfBuffers >= 0, s"DataStore: numberOfBuffers $numberOfBuffers must be >= 0")
 
-  var executionEngineOption: Option[ExecutionEngine] = None
-
-  def setExecutionEngine(executionEngine: ExecutionEngine): Unit = {
-    executionEngineOption = Some(executionEngine)
-
-    executionEngine.symbolsToWatch.foreach { symbolName =>
-      if (executionEngine.symbolTable.contains(symbolName)) {
-        watchList += executionEngine.symbolTable(symbolName)
-      }
-      else {
-        throw TreadleException(s"treadleOptions.symbols to watch has bad symbolName $symbolName")
-      }
-    }
-
-    setAssignmentDisplayModes()
-  }
-
-  def setAssignmentDisplayModes(): Unit = {
-    executionEngineOption.foreach { executionEngine =>
-      val watchList = executionEngine.symbolsToWatch.map { symbolName =>
-        executionEngine.symbolTable.get(symbolName) match {
-          case Some(symbol) =>
-            symbol
-          case _ =>
-            throw TreadleException(s"treadleOptions.symbols to watch has bad symbolName $symbolName")
-        }
-      }
-
-      val verbose = executionEngineOption.get.verbose
-      executionEngine.scheduler.combinationalAssigns.foreach { assigner =>
-        val render = watchList.contains(assigner.symbol)
-        assigner.setLeanMode(!verbose && !render)
-        assigner.setVerbose(verbose)
-        assigner.setRender(render)
-      }
-    }
-  }
-
   def numberOfInts: Int  = dataStoreAllocator.nextIndexFor(IntSize)
   def numberOfLongs: Int = dataStoreAllocator.nextIndexFor(LongSize)
   def numberOfBigs: Int  = dataStoreAllocator.nextIndexFor(BigSize)
-
-  val watchList: mutable.HashSet[Symbol] = new mutable.HashSet()
 
   val intData:   Array[Int]  = Array.fill(numberOfInts)(0)
   val longData:  Array[Long] = Array.fill(numberOfLongs)(0L)
@@ -633,16 +593,57 @@ trait DataPluginManager {
   }
 }
 
+trait ExecutionEngineLoader {
+  protected val watchList: mutable.HashSet[Symbol] = new mutable.HashSet()
+  protected var executionEngineOption: Option[ExecutionEngine] = None
+
+  def setExecutionEngine(executionEngine: ExecutionEngine): Unit = {
+    executionEngineOption = Some(executionEngine)
+
+    executionEngine.symbolsToWatch.foreach { symbolName =>
+      if (executionEngine.symbolTable.contains(symbolName)) {
+        watchList += executionEngine.symbolTable(symbolName)
+      }
+      else {
+        throw TreadleException(s"treadleOptions.symbols to watch has bad symbolName $symbolName")
+      }
+    }
+
+    setAssignmentDisplayModes()
+  }
+
+  private def setAssignmentDisplayModes(): Unit = {
+    executionEngineOption.foreach { executionEngine =>
+      val watchList = executionEngine.symbolsToWatch.map { symbolName =>
+        executionEngine.symbolTable.get(symbolName) match {
+          case Some(symbol) =>
+            symbol
+          case _ =>
+            throw TreadleException(s"treadleOptions.symbols to watch has bad symbolName $symbolName")
+        }
+      }
+
+      val verbose = executionEngineOption.get.verbose
+      executionEngine.scheduler.combinationalAssigns.foreach { assigner =>
+        val render = watchList.contains(assigner.symbol)
+        assigner.setLeanMode(!verbose && !render)
+        assigner.setVerbose(verbose)
+        assigner.setRender(render)
+      }
+    }
+  }
+}
+
 /** A DataBuffer is the a snapshot of a [[AbstractDataStore]] at a particular time. */
 trait DataBuffer extends DataReader {
   def getTime: Long
   def dump(dumpTime: Long): Unit
 }
 
-trait AbstractDataStore extends DataReader with DataWriter with DataSerializer with DataPluginManager {
+trait AbstractDataStore extends DataReader with DataWriter with DataSerializer with DataPluginManager
+    with ExecutionEngineLoader {
   var leanMode : Boolean
   val numberOfBuffers: Int
-  def setExecutionEngine(executionEngine: ExecutionEngine): Unit
   def saveData(time: Long): Unit
   def findBufferBeforeClockTransition(time: Long, clock: Symbol, prevClock: Symbol): Option[DataBuffer]
   def makeConstantAssigner(symbol: Symbol, value: Big, info: Info) : Assigner
