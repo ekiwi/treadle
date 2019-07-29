92e183241016a75f82283345b6e956288ec7938e
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-07-28 20:03:23 -0700

    test: test Riscv Mini with two different backends

--- src/test/scala/treadle/RiscVMiniSimpleSpec.scala
+++ src/test/scala/treadle/RiscVMiniSimpleSpec.scala
@@ -4,29 +4,29 @@ package treadle
 
 import firrtl.stage.FirrtlSourceAnnotation
 import org.scalatest.{FreeSpec, Matchers}
-import treadle.executable.{ClockInfo, StopException}
+import treadle.executable.{ClockInfo, FastExecutionBackend, SimpleExecutionBackend, StopException}
 
 //scalastyle:off magic.number
 class RiscVMiniSimpleSpec extends FreeSpec with Matchers {
-  "riscv-mini simple core test should run then stop" in {
-
-    val stream = getClass.getResourceAsStream("/core-simple.lo.fir")
-    val input =
-      scala.io.Source.fromInputStream(stream).getLines().mkString("\n")
-
-    val tester = TreadleTester(
-      Seq(
-        FirrtlSourceAnnotation(input),
-        ClockInfoAnnotation(
-          Seq(ClockInfo("clock", period = 10, initialOffset = 1))
-        )
-      )
-    )
-
+  val stream = getClass.getResourceAsStream("/core-simple.lo.fir")
+  val input = scala.io.Source.fromInputStream(stream).getLines().mkString("\n")
+  val annos = Seq(
+    FirrtlSourceAnnotation(input),
+    ClockInfoAnnotation(Seq(ClockInfo("clock", period = 10, initialOffset = 1)))
+  )
+  private def test(tester: TreadleTester) = {
     intercept[StopException] {
       tester.step(400)
     }
     tester.report()
     tester.engine.lastStopResult should be(Some(0))
+  }
+
+  "riscv-mini simple core test should run then stop" in {
+    test(TreadleTester(annos ++ Seq(TreadleBackendAnnotation(FastExecutionBackend))))
+  }
+
+  "riscv-mini simple core test should run then stop with SimpleExecutionBackend" in {
+    test(TreadleTester(annos ++ Seq(TreadleBackendAnnotation(SimpleExecutionBackend))))
   }
 }
